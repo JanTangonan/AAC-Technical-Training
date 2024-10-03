6f895b583a22f605923398d16376c8d7e892520b
Jumba <janthegreatxc@gmail.com>
2024-10-03 17:41:11 +0800

    Activity 1 - Completed

--- AAC-Technical-Training/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(() => {
-    // Highlight row on click
-    $('.cases-table tr').click(function () { 
-        $('.cases-table tr').removeClass('table-row-color');
-        $(this).addClass('table-row-color');
-    });
-
-    
-});
