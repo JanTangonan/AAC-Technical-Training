621ecc9d7bf6cb43c551d9c66622a7a7a617b401
Jumba <janthegreatxc@gmail.com>
2024-11-04 17:51:18 +0800

    Exercise 5 progress 3

---
+++ AAC-Technical-Training/ACT 5/nov 04/Exercise_Arvinn_Ajax.js
@@ -0,0 +1,243 @@
+$(document).ready(() => {
+    var selectedRow = null;
+    var selectedCaseKey = null;
+    var selectedOffenseCode = null;
+    var selectedDepartmentCode = null;
+    var initialValues = {};
+
+    bindCasesTable();
+    fetchDepartments();
+    fetchCharge();
+    rowHighlight();
+
+    /// Date picker UI widget
+    $('#incedentReportDate').datepicker();
+
+    $('#btnEdit').click(function () {
+        edit();
+    });
+
+    $('#btnSave').click(function () {
+        saveUpdate();
+    });
+
+    $('#btnCancel').click(function () {
+        cancelUpdate();
+    })
+
+    function bindCasesTable() {
+        /// <summary>Binds Cases Table data from DB via web method</summary>
+        /// <param>None.</param>
+        /// <return type = "void">Void.</return>
+        $.ajax({
+            type: "POST",
+            url: "Exercise_Arvinn_AjaxWS.asmx/GetTopCases",
+            data: '{}',
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (response) {
+                var data = JSON.parse(response.d);
+                var tableBody = $(".cases-table-body");
+                tableBody.empty(); 
+                $.each(data, function (i, item) {
+                    var row = "<tr data-case-key=' " + item.CASE_KEY +
+                        "' data-department-code='" + item.DEPARTMENT_CODE +
+                        "' data-offense-code='" + item.OFFENSE_CODE + "'>" +
+                        "<td>" + item.DEPARTMENT_CASE_NUMBER + "</td>" +
+                        "<td>" + item.DEPARTMENT_NAME + "</td>" +
+                        "<td>" + item.CHARGE + "</td>" +
+                        "<td>" + item.LAB_CASE + "</td>" +
+                        "<td>" + item.OFFENSE_DATE + "</td>" +
+                        "</tr>";
+                    
+                    tableBody.append(row);
+                });
+                console.log(response);
+            },
+            error: function (err) {
+                console.log("Error: ", err);
+            }
+        });
+    }
+
+    function fetchDepartments() {
+        /// <summary>Fetches List of Department objects from DB via web method</summary>
+        /// <param>None.</param>
+        /// <return type = "void">Void.</return>
+        $.ajax({
+            type: "POST",
+            url: "Exercise_Arvinn_AjaxWS.asmx/GetDepartments",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (response) {
+                populateDropdown("ddlDepartment", JSON.parse(response.d), "DEPARTMENT_CODE", "DEPARTMENT_NAME");
+            },
+            error: function (error) {
+                console.error("Error fetching Departments:", error);
+            }
+        });
+    }
+
+    function fetchCharge() {
+        /// <summary>Fetches List of Charge objects from DB via web method</summary>
+        /// <param>None.</param>
+        /// <return type = "void">Void.</return>
+        $.ajax({
+            type: "POST",
+            url: "Exercise_Arvinn_AjaxWS.asmx/GetCharges",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (response) {
+                populateDropdown("ddlCharge", JSON.parse(response.d), "OFFENSE_CODE", "OFFENSE_DESCRIPTION");
+            },
+            error: function (error) {
+                console.error("Error fetching Charges:", error);
+            }
+        });
+    }
+
+    function populateDropdown(dropdownId, items, valueField, textField) {
+        /// <summary>Populates dropdown list.</summary>
+        /// <param names = "dropdownId", "items", "valueField", "textField">Dropdown element, items, value field, text field</param>
+        /// <return type = "void">Void.</return>
+        var dropdown = document.getElementById(dropdownId);
+        dropdown.innerHTML = '<option value="">Select an option</option>';
+
+        items.forEach(function (item) {
+            var option = document.createElement("option");
+            option.value = item[valueField];
+            option.textContent = item[textField];
+            dropdown.appendChild(option);
+        });
+    }
+
+    function rowHighlight() {
+        /// <summary>Highlight and display selected row data in input elements upon clicking.</summary>
+        /// <param>None.</param>
+        /// <return type = "void">Void.</return>
+        $('.cases-table-body').on('click', 'tr', function () {
+            $('.cases-table-body tr').removeClass('table-row-color');
+            $(this).addClass('table-row-color');
+        
+            selectedRow = $(this);
+
+            selectedCaseKey = $(this).data("caseKey")
+            selectedOffenseCode = $(this).data("offenseCode")
+            selectedDepartmentCode = $(this).data("departmentCode")
+            var departmentCaseNumber = $(this).find('td:eq(0)').text();
+            var labCaseNumber = $(this).find('td:eq(3)').text();
+            var incedentReportDate = $(this).find('td:eq(4)').text();
+
+            $('#departmentCaseNumber').val(departmentCaseNumber);
+            $('#ddlDepartment').val(selectedDepartmentCode);
+            $('#ddlCharge').val(selectedOffenseCode);
+            $('#labCaseNumber').val(labCaseNumber);
+            $('#incedentReportDate').val(incedentReportDate);
+
+            storeInitialValue();
+        });
+    }
+
+    function edit() {
+        /// <summary>Enables input elements to edit table row data.</summary>
+        /// <param>None.</param>
+        /// <return type = "void">Void.</return>
+        if (selectedRow) {
+            disableInputButtons(false);
+
+            /// disable clicking on cases-table-row tr
+            $('.cases-table-body tr').not(selectedRow).addClass('disabled');
+        }
+    }
+
+    function saveUpdate() {
+        /// <summary>Saves update from input elements to DB.</summary>
+        /// <param>None.</param>
+        /// <return type = "void">Void.</return>        
+        if (selectedRow) {
+            const updatedData = {
+                CASE_KEY: selectedCaseKey,
+                DEPARTMENT_CASE_NUMBER: document.getElementById("departmentCaseNumber").value,
+                DEPARTMENT_CODE: document.getElementById("ddlDepartment").value,
+                OFFENSE_CODE: document.getElementById("ddlCharge").value,
+                LAB_CASE: document.getElementById("labCaseNumber").value,
+                OFFENSE_DATE: document.getElementById("incedentReportDate").value
+            };
+
+            $.ajax({
+                type: "POST",
+                url: "Exercise_Arvinn_AjaxWS.asmx/SaveData",
+                data: JSON.stringify(updatedData),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (response) {
+                    alert("Input Saved");
+                },
+                error: function (error) {
+                    console.error("Error saving data:", error);
+                }
+            });
+        }
+
+        bindCasesTable();
+        storeInitialValue();
+        disableInputButtons(true);
+        enableClick();
+        resetSelectedRow();
+    }
+
+    function cancelUpdate() {
+        /// <summary>Cancels update then restores initial value.</summary>
+        /// <param>None.</param>
+        /// <return type = "void">Void.</return>
+        restoreInitialValue();
+        disableInputButtons(true);
+        enableClick();
+    }
+
+    function storeInitialValue() {
+        /// <summary>Stores initial row value.</summary>
+        /// <param>None.</param>
+        /// <return type = "void">Void.</return>
+        $('.form-table input, .form-table select').each(function () {
+            var id = $(this).attr('id');
+            initialValues[id] = $(this).val();
+        });
+    }
+
+    function restoreInitialValue() {
+        /// <summary>Restores initial row value.</summary>
+        /// <param>None.</param>
+        /// <return type = "void">Void.</return>
+        $('.form-table input, .form-table select').each(function () {
+            var id = $(this).attr('id');
+            $(this).val(initialValues[id]);
+        });
+    }
+
+    function disableInputButtons(disabled) {
+        /// <summary>Disables input and button elements.</summary>
+        /// <param name = "disabled">Boolean parameter.</param>
+        /// <return type = "void">Void</return>
+        $("button").prop("disabled", disabled);
+        $("input").prop("disabled", disabled);
+        $("select").prop("disabled", disabled);
+        $('#btnEdit').prop('disabled', !disabled);
+    }
+
+    function enableClick() {
+        /// <summary>Enable clicking on table body rows.</summary>
+        /// <param>None.</param>
+        /// <return type = "void">Void.</return>
+        $('.cases-table-body tr').not(selectedRow).removeClass('disabled');
+    }
+
+    function resetSelectedRow() {
+        /// <summary>Resets Selected Row.</summary>
+        /// <param>None.</param>
+        /// <return type = "void">Void.</return>
+        $('.form-table input').val('');
+        $('.form-table select').prop('selectedIndex', 0);
+        selectedRow = null;
+    }
+});
