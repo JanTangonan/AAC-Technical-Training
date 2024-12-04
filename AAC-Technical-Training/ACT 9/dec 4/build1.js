6ba4522466494bcb83269217b9ab2f26d09077c1
Jumba <janthegreatxc@gmail.com>
2024-12-04 17:58:09 +0800

    Exercise 9 progress 1

---
+++ AAC-Technical-Training/ACT 9/dec 4/build1.js
@@ -0,0 +1,10 @@
+<table id="tblGrid"
+    data-read="Exercise_Arvin_Plugin2WS.asmx/GetTopCases"
+    data-columns='[
+                   { "name": "DEPARTMENT_CASE_NUMBER", "title": "Department Case #" },
+                   { "name": "DEPARTMENT_NAME", "title": "Department" },
+                   { "name": "CHARGE", "title": "Charge" },
+                   { "name": "LAB_CASE", "title": "Lab Case #" },
+                   { "name": "OFFENSE_DATE", "title": "Incident Report Date" }
+               ]'>
+</table>
