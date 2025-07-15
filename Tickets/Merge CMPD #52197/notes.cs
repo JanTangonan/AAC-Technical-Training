ca5d499981a6534cddbf88633a5856ed78af0af5
Jumba <janthegreatxc@gmail.com>
2025-07-15 17:59:10 +0800

    Merge CMPD #52197 progress

--- Tickets/Merge CMPD #52197/notes.cs
+++ Tickets/Merge CMPD #52197/notes.cs
@@ -157,3 +157,34 @@ protected void PLCDBPanel1_TextChanged(object sender, PLCDBPanelTextChangedEvent
         }
     }
 }
+
+private int GetAgeByOffenseDate(string dateOfBirth)
+{
+    int result = 0;
+    if (!string.IsNullOrEmpty(dateOfBirth))
+    {
+        PLCQuery qryCase = new PLCQuery(string.Format("SELECT OFFENSE_DATE FROM TV_LABCASE WHERE CASE_KEY = {0}", PLCSession.PLCGlobalCaseKey));
+        if (qryCase.Open() && qryCase.HasData() && !string.IsNullOrEmpty(qryCase.FieldByName("OFFENSE_DATE")))
+        {
+            DateTime offenseDate = DateTime.Parse(qryCase.FieldByName("OFFENSE_DATE"));
+            DateTime birthDate = DateTime.Parse(dateOfBirth);
+
+            result = offenseDate.Year - birthDate.Year;
+
+            if (offenseDate.Month < birthDate.Month || (offenseDate.Month == birthDate.Month && offenseDate.Day < birthDate.Day))
+                result--;
+
+            if (result > 0 && birthDate > offenseDate.AddYears(-result))
+                result--;
+        }
+    }
+
+    return result;
+}
+
+private int GetOldestJuvenileAge()
+{
+    int oldestJuvenile = PLCSession.SafeInt(PLCSession.GetLabCtrl("OLDEST_JUVENILE"), 0);
+
+    return oldestJuvenile > 0 ? oldestJuvenile : 18;
+}
---
+++ Tickets/Merge NYPD #106062/notes.sql
@@ -0,0 +1,20 @@
+select * from TV_CHEMSETUP
+where CHEM_CATEGORY = 'D'
+
+select * from TV_CHEMPANEL
+
+select * from TV_CHEMCAT
+
+TAB6Component.aspx
+CHEM_R_COMPONENT
+
+TAB7ServHistory.aspx
+CHEM_I_SERVICEHISTORY
+
+29	D	TAB11Receipts.aspx	Receive
+31	D	TAB6Component.aspx	Component	
+32	D	TAB7ServHistory.aspx	Service History	
+
+
+31	CHEM_R_COMPONENT	F	2
+32	CHEM_I_SERVICEHISTORY	F	1
