661f266f1b11c4d47237329550738f6929c57d25
Jumba <janthegreatxc@gmail.com>
2025-08-01 17:50:44 +0800

    Merge OHBCI #111367 Dev Testing

--- Tickets/Merge OHBCI #111367/notes.cs
+++ Tickets/Merge OHBCI #111367/notes.cs
@@ -3,3 +3,27 @@ if (
     PLCSession.GetDeptCtrlFlag("ALLOW_INQUIRY") != "T")
                 RemoveMenuItem("Case Inquiry");
                 
+private void SetDepartmentAccess()
+        {
+            bool showAllDepartments = PLCDBGlobal.instance.GetWebUserField("INQUIRY_ALL_DEPTS", PLCSession.PLCGlobalPrelogUser) == "T";
+
+            if (!showAllDepartments)
+            {
+                String codeCondition = "DEPARTMENT_CODE IN (" + PLCDBGlobal.instance.getUserDeptCodes() + ")";
+
+                //string codeCondition = PLCDBGlobal.instance.AssemblePrelogDeptCodeCondition(PLCSession.PLCGlobalAnalystDepartmentCode, PLCSession.PLCGlobalPrelogUser);
+                if (!string.IsNullOrEmpty(codeCondition))
+                {
+                    string origCodeCondition = dbpPrelogCaseSearch.GetFieldCodeCondition("DEPARTMENT_CODE");
+                    if (!string.IsNullOrEmpty(origCodeCondition))
+                        dbpPrelogCaseSearch.AppendPanelCodeCondition("DEPARTMENT_CODE", origCodeCondition, codeCondition);
+                    else
+                        dbpPrelogCaseSearch.SetPanelCodeCondition("DEPARTMENT_CODE", codeCondition);
+
+                    dbpPrelogCaseSearch.setpanelfield("DEPARTMENT_CODE", PLCSessionVars1.PLCGlobalAnalystDepartmentCode);
+
+                    if (!codeCondition.Contains(","))
+                        dbpPrelogCaseSearch.EnablePanelField("DEPARTMENT_CODE", false);
+                }
+            }
+        }
