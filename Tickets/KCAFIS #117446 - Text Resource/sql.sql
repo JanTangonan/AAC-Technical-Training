0e86d6621626d5d6cb2e106cefde3b774e7b3906
Jumba <janthegreatxc@gmail.com>
2025-08-28 17:52:14 +0800

    KCAFIS #117446 - additional change

---
+++ Tickets/KCAFIS #117446 - Text Resource/sql.sql
@@ -0,0 +1,6 @@
+INSERT INTO TV_TEXTRESOURCES (RESOURCE_ID, TEXT_DATA, RESOURCE_TYPE)
+VALUES 
+('Config_Tab3Depts_btnDeptInfo', 'Agency Information', 'Tab Departments - Dept Info Button'),
+('Config_Tab3Depts_DepartmentRolesCustomButton', 'Agency Roles', 'Tab Departments - Department Roles Custom Button'),
+('Config_Tab3Depts_Label1', 'Find Agencies', 'Tab Departments - Label1'),
+('Config_Tab3Depts_UploadToDepartmentCustomButton', 'Upload to Agency', 'Tab Departments - Upload to Department Custom Button');
