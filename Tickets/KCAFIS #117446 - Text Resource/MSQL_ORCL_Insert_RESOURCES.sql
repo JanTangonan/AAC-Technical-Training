19dc554d8d5aca935afbd54bf0ec43b5e9d969af
Jumba <janthegreatxc@gmail.com>
2025-08-29 17:40:52 +0800

    KCAFIS #117446 - additional changes

---
+++ Tickets/KCAFIS #117446 - Text Resource/MSQL_ORCL_Insert_RESOURCES.sql
@@ -0,0 +1,8 @@
+-- #117446 - Please change the title of "Departments" tab to "Agencies".
+
+INSERT INTO TV_TEXTRESOURCES (RESOURCE_ID, TEXT_DATA, RESOURCE_TYPE)
+VALUES 
+('Config_Tab3Depts_Label1', 'Find Agencies', 'Tab Departments - Label1'),
+('Config_Tab3Depts_btnDeptInfo', 'Agency Information', 'Tab Departments - Dept Info Button'),
+('Config_Tab3Depts_DepartmentRolesButton', 'Agency Roles', 'Tab Departments - Department Roles Button'),
+('Config_Tab3Depts_UploadToDepartmentButton', 'Upload to Agencies', 'Tab Departments - Upload to Department Button');
