64d31f0de7b53e3b6dfd572cd3107a0e0a03afc3
Jumba <janthegreatxc@gmail.com>
2025-10-23 17:46:10 +0800

    ABQ #17954 progress 2

--- Tickets/ABQ #17954/aspx.js
+++ Tickets/ABQ #17954/aspx.js
@@ -38,3 +38,41 @@ function isKitComponentsGridValid() {
 
     return gridIsValid;
 }
+
+var btn = document.getElementById('<%= btnSaveComp.ClientID %>')
+            btn.setAttribute("disabled", "disabled");
+
+
+function isKitComponentsGridValid() {
+
+    var btn = document.getElementById('<%= btnSaveComp.ClientID %>')
+    btn.setAttribute("disabled", "disabled");
+
+    var gridIsValid = true;
+    var dialogTitle = 'Kit Components Inventory';
+    var gridKitId = "<%= gvComponentKit.ClientID %>";
+    var kitGridJson = getKitComponentGridData(gridKitId);
+    var expirationDateRequired = getExpirationDatesRequired(kitGridJson);
+
+    var validationList = {
+        'expirationDateRequired': expirationDateRequired
+    };
+
+    console.log("expirationDateRequired.length: " + expirationDateRequired.length);
+    if (expirationDateRequired.length > 0) {
+        gridIsValid = false;
+    }
+
+    // not valid? show a message
+    if (!gridIsValid) {
+        message = '<div>Expiration date is required for the following kit item/s: <br/>' +
+            expirationDateRequired.join(', ') + '</div>';
+        showDialog(dialogTitle, message);
+
+        btn.setAttribute("disabled", "enabled");
+        return false;
+    }
+
+            <% --__doPostBack('<%= btnSaveComp.UniqueID %>', ''); --%>
+            return gridIsValid;
+}
