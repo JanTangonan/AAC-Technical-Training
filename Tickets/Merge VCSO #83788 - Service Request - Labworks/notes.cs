b0c4b8aa8fba0291db5c3527e2fbc5709d157988
Jumba <janthegreatxc@gmail.com>
2025-11-28 17:53:42 +0800

    testing

--- Tickets/Merge VCSO #83788 - Service Request - Labworks/notes.cs
+++ Tickets/Merge VCSO #83788 - Service Request - Labworks/notes.cs
@@ -73,4 +73,91 @@
             return true;
         }
 
-        
+public void EmptyMode()
+{
+    foreach (PanelRec pr in panelrecs)
+    {
+        if (pr.btn != null)
+            pr.btn.Enabled = false;
+
+        if (pr.tb != null)
+        {
+            pr.tb.BackColor = Color.LightGray;
+            pr.tb.Attributes["readonly"] = "readonly";
+            pr.tb.Text = "";
+
+            if (pr.time != null)
+            {
+                SetTimeTextBoxMode(pr, true);
+                pr.time.Text = "";
+            }
+        }
+        else if (pr.ddl != null)
+        {
+            if (pr.ddl.Items.FindByValue("") != null)
+                pr.ddl.SelectedValue = "";
+            SetDropDownListMode(pr.ddl, true);
+        }
+        else if (pr.rbl != null)
+        {
+            if (pr.rbl.Items.FindByValue("") != null)
+                pr.rbl.SelectedValue = "";
+            SetRadioButtonListMode(pr.rbl, true);
+            //pr.rbl.SelectedIndex = -1;
+        }
+        else if (pr.cb != null)
+        {
+            pr.cb.Checked = false;
+            pr.cb.Enabled = false;
+        }
+        else if (pr.chMultiLookup != null)
+        {
+            TextBox tb = pr.chMultiLookup.GetTextBox();
+            tb.BackColor = Color.LightGray;
+            tb.ReadOnly = true;
+
+            pr.chMultiLookup.SetText("");
+            pr.chMultiLookup.Enable(false);
+        }
+        else if (pr.chMultipickAc != null)
+        {
+            TextBox tb = pr.chMultipickAc.GetTextBox();
+            tb.BackColor = Color.LightGray;
+            tb.ReadOnly = true;
+
+            pr.chMultipickAc.SetText("");
+            pr.chMultipickAc.Enable(false);
+        }
+        else if (pr.chImgMultiLookup != null)
+        {
+            TextBox tb = pr.chImgMultiLookup.GetTextBox();
+            tb.BackColor = Color.LightGray;
+            tb.ReadOnly = true;
+
+            pr.chImgMultiLookup.SetText("");
+            pr.chImgMultiLookup.Enable(false);
+        }
+        else if (pr._codetable != "")
+        {
+            if (pr.chtb != null)
+            {
+                CodeHeadEnableControls(pr, false);
+                pr.chtb.Text = "";
+                pr.chlabel.Text = "";
+            }
+            else if (pr.chComboBox != null)
+            {
+                SetDropDownListMode(pr.chComboBox, true);
+                if (pr.chComboBox.Items.FindByValue("") != null)
+                    pr.chComboBox.SelectedValue = "";
+            }
+            else if (pr.chFlexBox != null)
+            {
+                pr.chFlexBox.ReadOnly = true;
+                pr.chFlexBox.SelectedValue = "";
+            }
+        }
+    }
+
+    SetDBPanelChildRequired();
+}
