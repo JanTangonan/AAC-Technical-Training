8864d04a8be71ef14a7364cbbac0f3a1dbe14cbc
Jumba <janthegreatxc@gmail.com>
2025-12-17 15:13:44 +0800

    OCHD #119042 testing, Merge KCAFIS #117446 code review

---
+++ Tickets/Merge KCAFIS #117446/MSQL_Insert_TEXTRESOURCES.sql
@@ -0,0 +1,9 @@
+-- #117446 - Please change the title of "Departments" tab to "Agencies".
+
+INSERT INTO TV_TEXTRESOURCES (RESOURCE_ID, TEXT_DATA, RESOURCE_TYPE)
+VALUES 
+('ConfigTAB3Depts.lblActive', 'Find Agency', 'Tab Departments - Label'),
+('ConfigTab3Depts.btnDeptInfo', 'Agency Information', 'Tab Departments - Dept Info Button'),
+('ButtonPanel.Department_Roles', 'Agency Roles', 'Tab Departments - Department Roles Button'),
+('ButtonPanel.Upload_To_Departments', 'Upload to Agencies', 'Tab Departments - Upload to Department Button'),
+('ButtonPanel.Dept_Picture_Mask', 'Agency Picture Mask', 'Tab Departments - Dept Picture Mask');
